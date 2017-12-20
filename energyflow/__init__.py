151b86babd6433a7687075795d3881ba3a2301fe
Patrick T. Komiske III <pkomiske@mit.edu>
2017-12-20 18:54:32 -0500

    update version

--- energyflow/__init__.py
+++ energyflow/__init__.py
@@ -8,4 +8,4 @@ from .multigraphs import *
 from .polynomials import *
 
 __all__ = ['multigraphs', 'polynomials'] + multigraphs.__all__ + polynomials.__all__
-__version__ = '0.3.6'
+__version__ = '0.4.0'
