51c459c4c92a4c45994ec4202ec3a60b9cef5ed7
Patrick T. Komiske III <pkomiske@mit.edu>
2018-03-03 16:56:43 -0500

    fixing setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ import energyflow
 setup(
 name='EnergyFlow',
 version=energyflow.__version__,
-description='Python implementation of the energy flow basis',
+description='Python implementation of the Energy Flow basis',
 author='Patrick T. Komiske III',
 author_email='pkomiske@mit.edu',
-url='https://github.com/pkomiske/EnergyFlow',
+url='https://pkomiske.github.io/EnergyFlow',
+project_urls={'GitHub': 'https://github.com/pkomiske/EnergyFlow'},
 license='GPL-3.0',
 install_requires=['numpy>=1.12.0', 'six>=1.10.0'],
 extras_require={'generation': ['python-igraph']},
