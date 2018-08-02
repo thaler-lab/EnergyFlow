76f7af54ec7a50d4bf1afe86a6cff382da182ed4
Patrick Komiske <pkomiske@gmail.com>
2018-08-02 03:07:32 -0400

    added dataset tools

---
+++ energyflow/datasets/qg_jets.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+import numpy as np
+
+from energyflow.utils import get_file
+
+def load(filename='QG_jets.npz', cache_dir=None):
+
+    fpath = get_file(filename, 
+                     url='https://www.dropbox.com/s/fclsl7pukcpobsb/QG_jets.npz?dl=1',
+                     file_hash='3f27a02eab06e8b83ccc9d25638021e6e24c9361341730961f9d560dee12c257',
+                     cache_dir=cache_dir)
+
+    f = np.load(fpath)
+    X, y = f['X'], f['y']
+    f.close()
+
+    return X, y
