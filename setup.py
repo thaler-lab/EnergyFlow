1782a88880e203482f90f978220d272ed3306fa6
ericmetodiev <eric.metodiev@gmail.com>
2018-10-18 11:26:25 -0400

    Updating EnergyFlow demo.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='EnergyFlow',
     version=energyflow.__version__,
     description='Python package for the Energy Flow suite of tools',
-    long_description='See the [EnergyFlow website](https://pkomiske.github.io/EnergyFlow).',
+    long_description='See the [EnergyFlow website](https://energyflow.network).',
     long_description_content_type='text/markdown',
     author='Patrick T. Komiske III',
     author_email='pkomiske@mit.edu',
