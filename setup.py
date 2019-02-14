a9cbb9dd8f9b07e2017c6e2ec27a6d89a6dcc845
Patrick Komiske <pkomiske@gmail.com>
2019-02-13 21:24:47 -0500

    v0.11.0

--- setup.py
+++ setup.py
@@ -3,13 +3,7 @@ import sys
 
 from setuptools import setup
 
-needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
-pytest_runner = ['pytest-runner'] if needs_pytest else []
-
 with open('energyflow/__init__.py', 'r') as f:
-     __version__ = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read()).group(1)
+    __version__ = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read()).group(1)
 
-setup(
-    version=__version__,
-    setup_requires=pytest_runner
-)
+setup(version=__version__)
