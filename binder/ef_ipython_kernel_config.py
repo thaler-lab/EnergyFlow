49eec8ac2a69ad349797fb6f131b08537233d46e
Patrick Komiske <pkomiske@gmail.com>
2019-08-08 23:42:36 -0400

    adding custom matplotlib to binder

---
+++ binder/ef_ipython_kernel_config.py
@@ -0,0 +1,12 @@
+# 
+c.InteractiveShellApp.matplotlib = 'inline'
+c.InlineBackend.figure_formats = {'retina'}
+c.InlineBackend.rc.update({'figure.figsize': (4,4),
+                           'figure.dpi': 120,
+                           'text.usetex': True,
+                           'text.latex.preview': True,
+                           'text.latex.preamble': [r'\usepackage{amsmath}'],
+                           'mathtext.fontset': 'cm',
+                           'mathtext.rm': 'serif',
+                           'font.family': 'serif'
+                           })
