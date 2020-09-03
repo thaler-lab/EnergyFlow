0f594feee401604b5040b74060fcd4fc38472cf4
Patrick T. Komiske III <pkomiske@mit.edu>
2020-09-02 21:52:17 -0400

    changed multiprocessing start method to be fork when possible

--- energyflow/__init__.py
+++ energyflow/__init__.py
@@ -1,6 +1,22 @@
 """The EnergyFlow package."""
 from __future__ import absolute_import
 
+# attempt to use 'fork' start method in multiprocessing
+import sys
+if sys.version_info[:2] >= (3, 4):
+    import multiprocessing, warnings
+    method = multiprocessing.get_start_method(True)
+    if method is None:
+        try:
+            multiprocessing.set_start_method('fork')
+        except:
+            method = ''
+    if method != 'fork':
+        warnings.warn('multiprocessing start method is {},'.format(multiprocessing.get_start_method())
+                      + ' EnergyFlow multicore functionality may not work properly')
+    del multiprocessing, warnings
+del sys
+
 # import top-level submodules
 from . import algorithms
 from . import base
@@ -30,4 +46,4 @@ __all__ = (datasets.__all__ +
            obs.__all__ +
            utils.__all__)
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
